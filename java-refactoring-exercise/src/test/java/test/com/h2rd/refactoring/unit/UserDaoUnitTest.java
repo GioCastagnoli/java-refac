d9ba7029c4cc2292b94cde1ac5a2e2e9d0841d0e
gcastall <gcastall@ZCO-9T34PC2.usersad.everis.int>
2020-04-16 17:07:18 -0400

    ....

--- java-refactoring-exercise/src/test/java/test/com/h2rd/refactoring/unit/UserDaoUnitTest.java
+++ java-refactoring-exercise/src/test/java/test/com/h2rd/refactoring/unit/UserDaoUnitTest.java
@@ -6,13 +6,10 @@ import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import com.h2rd.refactoring.usermanagement.User;
 import com.h2rd.refactoring.usermanagement.UserDao;
 
-@RunWith(MockitoJUnitRunner.class)
 public class UserDaoUnitTest {
 
 	UserDao userDao;
